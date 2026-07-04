aea402916cc00d2dd0a9bc8283457caf66d93cb9
Gayatri <24wh1a05x4@bvrithyderabad.edu.in>
2026-07-04 10:18:40 +0530

    day2 update

--- frontend/index.js
+++ frontend/index.js
@@ -22,5 +22,21 @@ function toggle(){
   document.getElementById("card-name").innerText=users[curUserId].name;
   document.getElementById("card-gender").innerText=users[curUserId].gender;
 }
+function getRandomUser(){
+  fetch("https://randomuser.me/api/")
+  .then(function(data){
+    return data.json();
+  })
+  .then(function(parsedData){
+    let gender=parsedData.results[0].gender;
+    let first=parsedData.results[0].name.first;
+    let last=parsedData.results[0].name.last;
+    let name=first+" "+last;
+    let imageurl=parsedData.results[0].picture.large;
+    document.getElementById("card-gender").innerText=gender;
+    document.getElementById("card-name").innerText=name;
+    document.getElementById("img").src=imageurl;
+  })
+}
 
 console.log("Hello from JS");
